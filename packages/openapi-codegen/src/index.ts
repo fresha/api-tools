d3a5af92c499ef5b9c437773aaacd82926a93ea9
Andriy Mykulyak <mykulyak@gmail.com>
2022-09-26 17:34:19 +0200

    Bump openapi-codegen version + register TS codegen

--- packages/openapi-codegen/src/index.ts
+++ packages/openapi-codegen/src/index.ts
@@ -1,3 +1,4 @@
+import { register as registerClientCodegen } from '@fresha/openapi-codegen-client-typescript';
 import { register as registerElixirCodegen } from '@fresha/openapi-codegen-server-elixir';
 import { register as registerNestJSCodegen } from '@fresha/openapi-codegen-server-nestjs';
 import yargs from 'yargs';
@@ -5,6 +6,7 @@ import { hideBin } from 'yargs/helpers';
 
 const argv = yargs(hideBin(process.argv)).usage('Usage');
 
+registerClientCodegen(argv);
 registerNestJSCodegen(argv);
 registerElixirCodegen(argv);
 
