5e08cb766aa9c30b112ce564820dd8e6b09e2235
Andriy Mykulyak <mykulyak@gmail.com>
2022-09-30 14:10:28 +0200

    Make .toHaveFormattedText() message more readable

--- tools/jest-config/src/setupEnv.ts
+++ tools/jest-config/src/setupEnv.ts
@@ -1,30 +1,49 @@
+import {
+  MatcherHintOptions,
+  matcherHint,
+  printDiffOrStringify,
+  printExpected,
+  printReceived,
+  stringify,
+} from 'jest-matcher-utils';
 import * as prettier from 'prettier';
 
 import type { SourceFile } from 'ts-morph';
 
+const EXPECTED_LABEL = 'Expected';
+const RECEIVED_LABEL = 'Received';
+
 expect.extend({
-  toHaveFormattedText: (
-    sourceFile: SourceFile,
-    textToCompare: string,
-  ): jest.CustomMatcherResult => {
+  toHaveFormattedText: (sourceFile: SourceFile, expectedText: string): jest.CustomMatcherResult => {
     const options: prettier.Options = {
       parser: 'typescript',
       singleQuote: true,
       trailingComma: 'all',
     };
-    const thisText = prettier.format(sourceFile.getText(), options);
-    const otherText = prettier.format(textToCompare, options);
+    const received = prettier.format(sourceFile.getText(), options);
+    const expected = prettier.format(expectedText, options);
 
-    if (thisText !== otherText) {
-      return {
-        pass: false,
-        message: () => `Source text differs. Expected '${otherText}', got '${thisText}'`,
-      };
-    }
-
-    return {
-      pass: true,
-      message: () => 'Source text is the same',
+    const matcherName = 'toHaveFormattedText';
+    const matcherOptions: MatcherHintOptions = {
+      comment: 'deep equality',
     };
+    const pass = received === expected;
+
+    const message = pass
+      ? () =>
+          // eslint-disable-next-line prefer-template
+          matcherHint(matcherName, undefined, undefined, matcherOptions) +
+          '\n\n' +
+          `Expected: not ${printExpected(expected)}\n` +
+          (stringify(expected) !== stringify(received)
+            ? `Received:     ${printReceived(received)}`
+            : '')
+      : () =>
+          // eslint-disable-next-line prefer-template
+          matcherHint(matcherName, undefined, undefined, matcherOptions) +
+          '\n\n' +
+          printDiffOrStringify(expected, received, EXPECTED_LABEL, RECEIVED_LABEL, true);
+
+    return { message, pass };
   },
 });
