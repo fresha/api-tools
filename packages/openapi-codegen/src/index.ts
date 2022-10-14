f27acbc8781cca00bde18cff969163f6dbc70726
Andriy Mykulyak <mykulyak@gmail.com>
2022-10-14 15:14:30 +0200

    Move CLI-specific code to a separate module

--- packages/openapi-codegen/src/index.ts
+++ packages/openapi-codegen/src/index.ts
@@ -1,13 +1,14 @@
 import { register as registerClientCodegen } from '@fresha/openapi-codegen-client-typescript';
 import { register as registerElixirCodegen } from '@fresha/openapi-codegen-server-elixir';
-import { register as registerNestJSCodegen } from '@fresha/openapi-codegen-server-nestjs';
+import * as nestJsCommand from '@fresha/openapi-codegen-server-nestjs/build/command';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
 const argv = yargs(hideBin(process.argv)).usage('Usage');
 
+argv.command(nestJsCommand);
+
 registerClientCodegen(argv);
-registerNestJSCodegen(argv);
 registerElixirCodegen(argv);
 
 argv.parseSync();
