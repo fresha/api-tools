294bf9cfce2a0400b808110a4db93d765dba22d9
Andriy Mykulyak <mykulyak@gmail.com>
2022-10-23 00:33:21 +0200

    Add @fresha/openapi-codegen-utils package

---
+++ packages/openapi-codegen-utils/src/openapi.test.ts
@@ -0,0 +1,110 @@
+import { OpenAPIFactory } from '@fresha/openapi-model/build/3.0.3';
+
+import {
+  getAPIName,
+  getOperationEntryKey,
+  getOperationEntryKeyOrThrow,
+  getOperationId,
+  getOperationIdOrThrow,
+  getRootUrl,
+  getRootUrlOrThrow,
+  significantNameParts,
+} from './openapi';
+
+test('significantNameParts', () => {
+  expect(significantNameParts('/')).toStrictEqual([]);
+  expect(significantNameParts('/x/y/z')).toStrictEqual(['x', 'y', 'z']);
+  expect(significantNameParts('/{id}/a/{q}')).toStrictEqual(['a']);
+});
+
+test('getAPIName', () => {
+  const openapi = OpenAPIFactory.create('Some Api', '0.1.2');
+  expect(getAPIName(openapi)).toBe('Some Api');
+});
+
+test('getRootUrl', () => {
+  const openapi = OpenAPIFactory.create();
+
+  expect(getRootUrl(openapi)).toBeUndefined();
+
+  openapi.paths.setExtension('root-url', 'http://www.example.com');
+  expect(getRootUrl(openapi)).toBe('http://www.example.com');
+
+  openapi.paths.setExtension('root-url', 234);
+  expect(() => getRootUrl(openapi)).toThrow();
+});
+
+test('getRootUrlOrThrow', () => {
+  const openapi = OpenAPIFactory.create();
+
+  expect(() => getRootUrlOrThrow(openapi)).toThrow();
+
+  openapi.paths.setExtension('root-url', '');
+  expect(() => getRootUrlOrThrow(openapi)).toThrow();
+
+  openapi.paths.setExtension('root-url', 'https://www.example.com');
+  expect(getRootUrlOrThrow(openapi)).toBe('https://www.example.com');
+});
+
+test('getOperationEntryKey', () => {
+  const openapi = OpenAPIFactory.create();
+  const operation = openapi.setPathItem('/op').setOperation('get');
+
+  expect(getOperationEntryKey(operation)).toBeUndefined();
+
+  operation.setExtension('entry-key', 'https://www.example.com');
+  expect(getOperationEntryKey(operation)).toBe('https://www.example.com');
+
+  operation.setExtension('entry-key', null);
+  expect(() => getOperationEntryKey(operation)).toThrow();
+});
+
+test('getOperationEntryKeyOrThrow', () => {
+  const openapi = OpenAPIFactory.create();
+  const operation = openapi.setPathItem('/op').setOperation('get');
+
+  expect(() => getOperationEntryKeyOrThrow(operation)).toThrow();
+
+  operation.setExtension('entry-key', 'https://www.example.com');
+  expect(getOperationEntryKeyOrThrow(operation)).toBe('https://www.example.com');
+
+  operation.setExtension('entry-key', '');
+  expect(() => getOperationEntryKeyOrThrow(operation)).toThrow();
+});
+
+describe('getOperationId', () => {
+  let operation = OpenAPIFactory.create().setPathItem('/op1').setOperation('get');
+
+  beforeEach(() => {
+    operation = OpenAPIFactory.create().setPathItem('/op1').setOperation('get');
+  });
+
+  test('undefined', () => {
+    expect(getOperationId(operation)).toBeUndefined();
+  });
+
+  test('operationId', () => {
+    operation.operationId = 'op1';
+    expect(getOperationId(operation)).toBe('op1');
+  });
+
+  test('x-codegen-operation', () => {
+    operation.setExtension('codegen-operation', 'op2');
+    expect(getOperationId(operation)).toBe('op2');
+  });
+
+  test('operationId has priority over x-codegen-operation', () => {
+    operation.operationId = 'op1';
+    operation.setExtension('codegen-operation', 'op2');
+    expect(getOperationId(operation)).toBe('op1');
+  });
+});
+
+test('getOperationIdOrThrow', () => {
+  const operation = OpenAPIFactory.create().setPathItem('/op1').setOperation('get');
+
+  expect(() => getOperationIdOrThrow(operation)).toThrow();
+
+  operation.operationId = 'op3';
+  expect(getOperationIdOrThrow(operation)).toBe('op3');
+});
