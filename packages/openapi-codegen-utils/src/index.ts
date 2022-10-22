db1eb688247ccbbd83a66eea9e58d05dad996a81
Andriy Mykulyak <mykulyak@gmail.com>
2022-10-23 00:33:21 +0200

    Add @fresha/openapi-codegen-utils package

---
+++ packages/openapi-codegen-utils/src/index.ts
@@ -0,0 +1,4 @@
+export * from './logging';
+export * from './openapi';
+export * from './string';
+export * from './tsMorph';
---
+++ packages/openapi-codegen-utils/src/string.test.ts
@@ -0,0 +1,27 @@
+import { titleCase, commonStringPrefix } from './string';
+
+test('titleCase', () => {
+  expect(titleCase('forget-me-not')).toBe('ForgetMeNot');
+  expect(titleCase('forgetMeNot')).toBe('ForgetMeNot');
+});
+
+test('empty array', () => {
+  expect(() => commonStringPrefix([])).toThrow();
+});
+
+test('1 elemnt', () => {
+  expect(commonStringPrefix(['/prefix'])).toBe('/prefix');
+});
+
+test('2+ elements', () => {
+  expect(commonStringPrefix(['/b/x', '/b/r'])).toBe('/b');
+  expect(commonStringPrefix(['/p/refix', '/p/fx', '/p/dz'])).toBe('/p');
+});
+
+test('leading /', () => {
+  expect(commonStringPrefix(['/items', 'items'])).toBe('/items');
+});
+
+test('only complete URL parts', () => {
+  expect(commonStringPrefix(['/prefix', '/pfx', '/pdz'])).toBe('');
+});
