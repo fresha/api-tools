ead664c154ea90b723d8044d01326e4793667f17
Andriy Mykulyak <mykulyak@gmail.com>
2022-11-02 13:07:05 +0100

    Add example NPM package for testing TS code generators

--- examples/codegen/run.sh
+++ examples/codegen/run.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-npx fresha-openapi-codegen server-elixir \
+# npx fresha-openapi-codegen server-elixir \
+#   --input ../openapi-codegen-test-api.yaml \
+#   --output ./elixir_api \
+#   --phoenix-app elixir_api_web \
+#   --test-factory-module ElixirApi.Factory \
+#   --json-api \
+#   --verbose
+
+npx fresha-openapi-codegen server-mock \
   --input ../openapi-codegen-test-api.yaml \
-  --output ./elixir_api \
-  --phoenix-app elixir_api_web \
-  --test-factory-module ElixirApi.Factory \
+  --output ./mock_api \
   --json-api \
   --verbose
