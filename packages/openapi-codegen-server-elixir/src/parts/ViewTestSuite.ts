3d7dfc1ca492245a3a292518bde175cae8631b35
Andriy Mykulyak <mykulyak@gmail.com>
2022-11-02 11:59:21 +0100

    Generate stubs for view tests

--- packages/openapi-codegen-server-elixir/src/parts/ViewTestSuite.ts
+++ packages/openapi-codegen-server-elixir/src/parts/ViewTestSuite.ts
@@ -6,16 +6,21 @@ export class ViewTestSuite {
   readonly context: Context;
   readonly moduleName: string;
   readonly sourceFile: SourceFile;
+  readonly actions: string[];
 
   constructor(context: Context, moduleName: string) {
     this.context = context;
     this.moduleName = moduleName;
     this.sourceFile = this.context.project.createViewTestFile(moduleName);
+    this.actions = [];
   }
 
   collectData(view: View): void {
     this.context.logger.info(`Generating code for view tests "${this.moduleName}"`);
-    this.context.logger.debug(view);
+
+    for (const action of view.actions) {
+      this.actions.push(action.getName());
+    }
   }
 
   generateCode(): void {
@@ -24,7 +29,15 @@ export class ViewTestSuite {
     this.sourceFile.writeDefmodule(this.moduleName, () => {
       this.sourceFile.writeUse(this.context.project.getModuleName('ConnCase'), 'async: true');
       this.sourceFile.writeUse('Jabbax.Document');
-      this.sourceFile.newLine();
+
+      for (const action of this.actions) {
+        this.sourceFile.newLine();
+        this.sourceFile.writeTestFunction(`renders ${action}.json-api`, () => {
+          this.sourceFile.writeLine('# TODO prepare data');
+          this.sourceFile.writeLine('# TODO render view');
+          this.sourceFile.writeLine('# TODO verify returned data');
+        });
+      }
     });
   }
 }
