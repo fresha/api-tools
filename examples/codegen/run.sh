8ce7bf8369fc4c0c25248d765d7d742ef61ffe87
Andriy Mykulyak <mykulyak@gmail.com>
2022-11-04 10:44:15 +0100

    Use globally-installed version of code generator

--- examples/codegen/run.sh
+++ examples/codegen/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-npx fresha-openapi-codegen server-elixir \
+# remove -g option if you want to develop locally
+npx -g fresha-openapi-codegen server-elixir \
   --input ../openapi-codegen-test-api.yaml \
   --output ./elixir_api \
   --phoenix-app elixir_api_web \
