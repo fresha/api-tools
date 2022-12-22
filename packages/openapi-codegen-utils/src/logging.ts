5415a8232e8d47ca9dfad93a85c1d2361a66eae8
Andriy Mykulyak <mykulyak@gmail.com>
2022-12-22 20:38:46 +0100

    Add shared createContext and createConsole functions

--- packages/openapi-codegen-utils/src/logging.ts
+++ packages/openapi-codegen-utils/src/logging.ts
@@ -1,3 +1,6 @@
+import { Console } from 'console';
+import { Writable } from 'stream';
+
 import * as winston from 'winston';
 
 export type Logger = winston.Logger;
@@ -7,4 +10,27 @@ export const createLogger = (verbose: boolean): Logger => {
     level: verbose ? 'info' : 'warn',
     transports: [new winston.transports.Console({ format: winston.format.simple() })],
   });
+};
+
+class NullWritable extends Writable {
+  // eslint-disable-next-line class-methods-use-this, no-underscore-dangle
+  _write(_chunk: unknown, _encoding: string, callback: (error?: Error | null) => void): void {
+    callback();
+  }
+
+  // eslint-disable-next-line class-methods-use-this, no-underscore-dangle
+  _writev(
+    _chunks: Array<{ chunk: unknown; encoding: string }>,
+    callback: (error?: Error | null) => void,
+  ): void {
+    callback();
+  }
+}
+
+export const createConsole = (verbose: boolean): Console => {
+  if (verbose) {
+    return new Console(process.stdout, process.stderr);
+  }
+  const devNull = new NullWritable();
+  return new Console(devNull, devNull);
 };
